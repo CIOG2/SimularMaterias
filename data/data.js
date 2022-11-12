964c7110d7de380b8faf225f6cfa117bc83bbf47
Carlos Ivan <carlos.ivan10043@gmail.com>
2022-11-12 16:00:23 -0700

    Archivos base

---
+++ data/data.js
@@ -0,0 +1,7 @@
+import partial1 from "./partial1.js";
+
+const data = [
+    ...partial1,
+];
+
+export default data;
---
+++ data/partial1.js
@@ -0,0 +1,49 @@
+const partial1 = [
+    {
+        id: 1,
+        name: 'Calculo diferencial',
+        parent: false,
+        children: [],
+        estatus: false,
+        partial: 1
+    },
+    {
+        id: 2,
+        name: 'Fundamentos de programación',
+        parent: false,
+        children: [],
+        estatus: false,
+        partial: 1
+    },
+    {
+        id: 3,
+        name: 'Taller de etica',
+        parent: false,
+        children: [],
+        estatus: false,
+        partial: 1
+    },    {
+        id: 4,
+        name: 'Matematicas discretas',
+        parent: false,
+        children: [],
+        estatus: false,
+        partial: 1
+    },    {
+        id: 5,
+        name: 'Taller de administración',
+        parent: false,
+        children: [],
+        estatus: false,
+        partial: 1
+    },    {
+        id: 6,
+        name: 'Fundamentos de investigación',
+        parent: false,
+        children: [],
+        estatus: false,
+        partial: 1
+    },
+]
+
+export default partial1 ;
